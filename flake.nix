10a62fdcee053b5279127dc86dc2910697d9e935
Matteo Pacini <m+github@matteopacini.me>
2025-08-25 20:18:57 +0100

    Limited Flake to x86_64-linux

--- flake.nix
+++ flake.nix
@@ -3,74 +3,70 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/25.05";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
     {
       self,
       nixpkgs,
-      flake-utils,
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        pkgsi486 = import nixpkgs {
-          localSystem = {
-            system = system;
-          };
-          crossSystem = {
-            config = "i486-unknown-linux-musl";
-            gcc = {
-              arch = "i486";
-              tune = "i486";
-            };
-          };
-          config = {
-            allowUnsupportedSystem = true;
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+      pkgsi486 = import nixpkgs {
+        localSystem = {
+          system = system;
+        };
+        crossSystem = {
+          config = "i486-unknown-linux-musl";
+          gcc = {
+            arch = "i486";
+            tune = "i486";
           };
         };
-        kernel = pkgs.callPackage ./packages/kernel { inherit pkgsi486; };
-        busybox = import ./packages/busybox { inherit pkgs pkgsi486; };
-        floppyA = import ./packages/floppies/A {
-          inherit pkgs pkgsi486;
-          kerneli486 = kernel;
-          busyboxi486Static = busybox;
+        config = {
+          allowUnsupportedSystem = true;
         };
-      in
-      {
+      };
+      kernel = pkgs.callPackage ./packages/kernel { inherit pkgsi486; };
+      busybox = import ./packages/busybox { inherit pkgs pkgsi486; };
+      floppyA = import ./packages/floppies/A {
+        inherit pkgs pkgsi486;
+        kerneli486 = kernel;
+        busyboxi486Static = busybox;
+      };
+    in
+    {
 
-        packages = {
-          zplinux-floppy-a = floppyA;
-        };
+      packages.x86_64-linux = {
+        zplinuxFloppyA = floppyA;
+      };
 
-        apps = {
-          default =
-            let
-              script = pkgs.writeShellScriptBin "run-in-qemu" ''
-                exec qemu-system-i386 \
-                  -cpu 486 -m 16M \
-                  -drive file=${floppyA}/zplinux-floppy-a.img,if=floppy,format=raw,readonly=on
-              '';
-            in
-            {
-              type = "app";
-              program = "${script}/bin/run-in-qemu";
-            };
-          checkFloppyADiskSpace =
-            let
-              script = pkgs.writeShellScriptBin "check-floppy-a-disk-space" ''
-                cat ${floppyA}/disk-usage.txt
-              '';
-            in
-            {
-              type = "app";
-              program = "${script}/bin/check-floppy-a-disk-space";
-            };
+      apps.x86_64-linux = {
+        default =
+          let
+            script = pkgs.writeShellScriptBin "run-in-qemu" ''
+              qemu-system-i386 \
+                -cpu 486 -m 16M \
+                -drive file=${floppyA}/zplinux-floppy-a.img,if=floppy,format=raw,readonly=on
+            '';
+          in
+          {
+            type = "app";
+            program = "${script}/bin/run-in-qemu";
+          };
+        checkFloppyADiskSpace =
+          let
+            script = pkgs.writeShellScriptBin "check-floppy-a-disk-space" ''
+              cat ${floppyA}/disk-usage.txt
+            '';
+          in
+          {
+            type = "app";
+            program = "${script}/bin/check-floppy-a-disk-space";
+          };
 
-        };
+      };
 
-      }
-    );
+    };
 }
